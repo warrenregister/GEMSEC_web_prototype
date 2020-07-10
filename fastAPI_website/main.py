44df0486f206df03b133b43ae8eac686d1710398
Chris P <chrisp1877@gmail.com>
2020-07-10 15:29:48 -0700

    fixed typo

--- fastAPI_website/main.py
+++ fastAPI_website/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-mport sqlalchemy
 import uvicorn
 
 app = FastAPI(debug=True)
